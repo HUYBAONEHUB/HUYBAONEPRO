f83d0fe027ca4f1435094a881050a5a7e3c46d90
HUYBAONEHUB <sicolawinnyne@gmail.com>
2025-08-29 11:36:48 +0700

    HUYBAONE_HUB
    Ohno

---
+++ HUYBAONE_HUB.lua
@@ -0,0 +1,9 @@
+
+-- Script gốc giữ nguyên chức năng
+-- Chỉ thay đổi ID ảnh và tên
+
+local imageID = "87260929956826"
+local hubName = "HUYBAONE_HUB"
+
+-- Các phần khác của script giữ nguyên
+print("Script đã được cập nhật với ID: "..imageID.." và tên: "..hubName)
